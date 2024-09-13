e370e6776ba4e01d4801b1f4adb636324637a386
Aidan Bleser <aidanbleser35@gmail.com>
2024-09-12 19:41:59 -0500

    Happy with API working on documentation

--- blocks/types/new-result.ts
+++ blocks/types/new-result.ts
@@ -1,16 +1,39 @@
-// THIS IS A DRAFT
-
 type _Result<T, E> = [val: T, err: null] | [val: null, err: E];
 
-export class Result<T, E> {
-	result: _Result<T, E>;
+/** Result allows you to show to a consumer that a function might throw and force them to handle it.
+ * 
+ *  `T` Value type
+ * 
+ *  `E` Error type
+ * 
+ * 	## Examples
+ * 
+ * ```ts
+ * const functionThatMightThrow = (): Result<string, string> => Ok("Hello, World!");
+ * 
+ * const result = functionThatMightThrow();
+ * 
+ * console.log(result.unwrap()); // "Hello, World!"
+ * ```
+ */
+class Result<T, E> {
+	private _result: _Result<T, E>;
 
 	constructor(result: _Result<T, E>) {
-		this.result = result;
+		this._result = result;
 	}
 
+	/** Allows you to run callbacks based on the result.
+	 * 
+	 * @param success callback to be run when result is success
+	 * @param failure callback to be run when result is failure
+	 * @returns 
+	 * 
+	 * ## Examples
+	 * 
+	 */
 	match<A, B = A>(success: (val: T) => A, failure: (err: E) => B): A | B {
-		const [val, err] = this.result;
+		const [val, err] = this._result;
 
 		if (err != null) {
 			return failure(err);
@@ -19,18 +42,36 @@ export class Result<T, E> {
 		// TS is unfortunately not smart enough to figure this out
 		return success(val as T);
 	}
+	map<A>(fn: (val: T) => A): Result<A, E> {
+		return this.match(
+			(val) => Ok(fn(val)),
+			(err) => Err(err)
+		);
+	}
 
-	unwrap(): T {
-		const [val, err] = this.result;
+	mapOr<A>(defaultVal: A, fn: (val: T) => A): A {
+		return this.match(
+			(val) => fn(val),
+			(_) => defaultVal
+		);
+	}
 
-		if (err != null) throw new Error(err.toString());
+	mapErr<A>(fn: (err: E) => A): Result<T, A> {
+		return this.match(
+			(val) => Ok(val),
+			(err) => Err(fn(err))
+		);
+	}
 
-		// TS is unfortunately not smart enough to figure this out
-		return val as T;
+	mapErrOr<A>(defaultVal: A, fn: (err: E) => A): A {
+		return this.match(
+			(_) => defaultVal,
+			(err) => fn(err)
+		);
 	}
 
 	isOk(): boolean {
-		const [_, err] = this.result;
+		const [_, err] = this._result;
 
 		if (err != null) return false;
 
@@ -38,15 +79,39 @@ export class Result<T, E> {
 	}
 
 	isErr(): boolean {
-		const [_, err] = this.result;
+		const [_, err] = this._result;
 
 		if (err == null) return true;
 
 		return false;
 	}
 
+	unwrap(): T {
+		const [val, err] = this._result;
+
+		if (err != null) throw new Error(err.toString());
+
+		// TS is unfortunately not smart enough to figure this out
+		return val as T;
+	}
+
+	unwrapOr(defaultVal: T): T {
+		return this.match(
+			(val) => val,
+			(_) => defaultVal
+		);
+	}
+
+	expectErr(message: string): E {
+		const [_, err] = this._result;
+
+		if (err == null) throw new Error(message);
+
+		return err;
+	}
+
 	expect(message: string): T {
-		const [val, err] = this.result;
+		const [val, err] = this._result;
 
 		if (err != null) throw new Error(message);
 
@@ -62,20 +127,3 @@ export const Ok = <T>(val: T): Result<T, never> => {
 export const Err = <E>(err: E): Result<never, E> => {
 	return new Result<never, E>([null, err]);
 };
-
-const functionThatCanFail = (): Result<number, string> => {
-	return Ok(10);
-};
-
-const res = functionThatCanFail();
-
-res.isOk();
-
-const value = res.match(
-	(val) => val,
-	(_) => {
-		throw new Error("oops");
-	}
-);
-
-console.log(value);
