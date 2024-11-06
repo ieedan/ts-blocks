1484ed9041c04f515e80751c5f826fa19f1fa12d
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-06 17:02:02 -0600

    Update ipv4-address.test.ts

--- blocks/utilities/ipv4-address.test.ts
+++ blocks/utilities/ipv4-address.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from 'vitest';
-import { parse, validate, formatToString } from './ipv4-address';
+import { describe, expect, it } from 'vitest';
+import { formatToString, parse, validate } from './ipv4-address';
 
 describe('parse', () => {
 	it(`Allows '.' separator.`, () => {
