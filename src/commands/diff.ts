98e57de56fe1b8374b8ae6ab465fdf3b69f48ca8
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-18 06:25:37 -0600

    refactors diff command to split logic

--- src/commands/diff.ts
+++ src/commands/diff.ts
@@ -1,22 +1,23 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import { cancel, confirm, intro, isCancel, outro, spinner } from '@clack/prompts';
-import color from 'chalk';
-import { Command, program } from 'commander';
-import { type Change, diffLines } from 'diff';
-import * as v from 'valibot';
-import { context } from '..';
-import { arraySum } from '../blocks/utilities/array-sum';
-import * as lines from '../blocks/utilities/lines';
-import { leftPadMin } from '../blocks/utilities/pad';
-import { getConfig } from '../config';
-import { OUTPUT_FILE } from '../utils';
-import type { Block } from '../utils/build';
-import { getInstalledBlocks } from '../utils/get-installed-blocks';
-import { getWatermark } from '../utils/get-watermark';
-import * as gitProviders from '../utils/git-providers';
+import fs from "node:fs";
+import path from "node:path";
+import { cancel, confirm, intro, isCancel, outro, spinner } from "@clack/prompts";
+import color from "chalk";
+import { Command, program } from "commander";
+import { type Change, diffLines } from "diff";
+import * as v from "valibot";
+import { context } from "..";
+import { arraySum } from "../blocks/utilities/array-sum";
+import * as lines from "../blocks/utilities/lines";
+import { leftPadMin } from "../blocks/utilities/pad";
+import { getConfig } from "../config";
+import { OUTPUT_FILE } from "../utils";
+import type { Block } from "../utils/build";
+import { getInstalledBlocks } from "../utils/get-installed-blocks";
+import { getWatermark } from "../utils/get-watermark";
+import * as gitProviders from "../utils/git-providers";
+import { formatDiff } from "../utils/diff";
 
-const L = color.gray('│');
+const L = color.gray("│");
 
 const schema = v.object({
 	allow: v.boolean(),
@@ -27,14 +28,14 @@ const schema = v.object({
 
 type Options = v.InferInput<typeof schema>;
 
-const diff = new Command('diff')
-	.description('Compares local blocks to the blocks in the provided repository.')
-	.option('-A, --allow', 'Allow ts-blocks to download code from the provided repo.', false)
-	.option('-E, --expand', 'Expands the diff so you see everything.', false)
-	.option('--repo <repo>', 'Repository to download the blocks from.')
+const diff = new Command("diff")
+	.description("Compares local blocks to the blocks in the provided repository.")
+	.option("-A, --allow", "Allow ts-blocks to download code from the provided repo.", false)
+	.option("-E, --expand", "Expands the diff so you see everything.", false)
+	.option("--repo <repo>", "Repository to download the blocks from.")
 	.option(
-		'--max-unchanged <number>',
-		'Maximum unchanged lines that will show without being collapsed.',
+		"--max-unchanged <number>",
+		"Maximum unchanged lines that will show without being collapsed.",
 		(val) => Number.parseInt(val), // this is such a dumb api thing
 		3
 	)
@@ -47,7 +48,7 @@ const diff = new Command('diff')
 type RemoteBlock = Block & { sourceRepo: gitProviders.Info };
 
 const _diff = async (options: Options) => {
-	intro(`${color.bgBlueBright(' ts-blocks ')}${color.gray(` v${context.package.version} `)}`);
+	intro(`${color.bgBlueBright(" ts-blocks ")}${color.gray(` v${context.package.version} `)}`);
 
 	const loading = spinner();
 
@@ -65,17 +66,17 @@ const _diff = async (options: Options) => {
 
 	if (!options.allow && options.repo) {
 		const result = await confirm({
-			message: `Allow ${color.cyan('ts-blocks')} to download and run code from ${color.cyan(options.repo)}?`,
+			message: `Allow ${color.cyan("ts-blocks")} to download and run code from ${color.cyan(options.repo)}?`,
 			initialValue: true,
 		});
 
 		if (isCancel(result) || !result) {
-			cancel('Canceled!');
+			cancel("Canceled!");
 			process.exit(0);
 		}
 	}
 
-	loading.start(`Fetching blocks from ${color.cyan(repoPaths.join(', '))}`);
+	loading.start(`Fetching blocks from ${color.cyan(repoPaths.join(", "))}`);
 
 	for (const repo of repoPaths) {
 		const providerInfo: gitProviders.Info = (await gitProviders.getProviderInfo(repo)).match(
@@ -103,7 +104,7 @@ const _diff = async (options: Options) => {
 		}
 	}
 
-	loading.stop(`Retrieved blocks from ${color.cyan(repoPaths.join(', '))}`);
+	loading.stop(`Retrieved blocks from ${color.cyan(repoPaths.join(", "))}`);
 
 	const installedBlocks = getInstalledBlocks(blocksMap, config);
 
@@ -130,7 +131,7 @@ const _diff = async (options: Options) => {
 
 			for (const file of block.files) {
 				// skip test files if not included
-				if (!config.includeTests && file.endsWith('test.ts')) continue;
+				if (!config.includeTests && file.endsWith("test.ts")) continue;
 
 				process.stdout.write(`${L}\n`);
 
@@ -153,164 +154,49 @@ const _diff = async (options: Options) => {
 					localPath = path.join(config.path, block.category, block.name, file);
 				}
 
-				let fileContent = '';
+				let fileContent = "";
 				if (fs.existsSync(localPath)) {
 					fileContent = fs.readFileSync(localPath).toString();
 				}
 
 				const changes = diffLines(fileContent, `${watermark}${remoteContent}`);
 
-				printDiff(
-					path
-						.join(
-							`${providerInfo.name}/${providerInfo.owner}/${providerInfo.repoName}`,
-							sourcePath
-						)
-						.replaceAll('\\', '/'),
-					localPath.replaceAll('\\', '/'),
+				const from = path
+					.join(`${providerInfo.name}/${providerInfo.owner}/${providerInfo.repoName}`, sourcePath)
+					.replaceAll("\\", "/");
+
+				const to = localPath.replaceAll("\\", "/");
+
+				const formattedDiff = formatDiff({
+					from,
+					to,
 					changes,
-					options
-				);
+					expand: options.expand,
+					maxUnchanged: options.maxUnchanged,
+					prefix: () => `${L} `,
+					onUnchanged: ({ from, to, prefix }) =>
+						`${prefix?.() ?? ""}${color.cyan(from)} → ${color.gray(to)} ${color.gray("(unchanged)")}\n`,
+					intro: ({ from, to, changes, prefix }) => {
+						const totalChanges = changes.filter((a) => a.added).length;
+
+						return `${prefix?.() ?? ""} ${color.cyan(from)} → ${color.gray(to)} (${totalChanges} change${
+							totalChanges === 1 ? "" : "s"
+						})\n${prefix?.() ?? ""}\n`;
+					},
+				});
+
+				process.stdout.write(formattedDiff);
 			}
 
 			break;
 		}
 
 		if (!found) {
-			program.error(
-				color.red(`Invalid block! ${color.bold(blockSpecifier)} does not exist!`)
-			);
+			program.error(color.red(`Invalid block! ${color.bold(blockSpecifier)} does not exist!`));
 		}
 	}
 
-	outro(color.green('All done!'));
-};
-
-const printDiff = (specifier: string, localPath: string, changes: Change[], options: Options) => {
-	const length = arraySum(changes, (change) => change.count ?? 0).toString().length + 1;
-
-	let lineOffset = 0;
-
-	if (changes.length === 1 && !changes[0].added && !changes[0].removed) {
-		process.stdout.write(
-			`${L}  ${color.cyan(specifier)} → ${color.gray(localPath)} ${color.gray('(unchanged)')}\n`
-		);
-		return;
-	}
-
-	const totalChanges = changes.filter((a) => a.added).length;
-
-	process.stdout.write(
-		`${L}  ${color.cyan(specifier)} → ${color.gray(localPath)} (${totalChanges} change${
-			totalChanges === 1 ? '' : 's'
-		})\n${L}\n`
-	);
-
-	/** Provides the line number prefix */
-	const linePrefix = (line: number): string =>
-		color.gray(`│ ${leftPadMin(`${line + 1 + lineOffset} `, length)} `);
-
-	for (let i = 0; i < changes.length; i++) {
-		const change = changes[i];
-
-		const hasPreviousChange = changes[i - 1]?.added || changes[i - 1]?.removed;
-		const hasNextChange = changes[i + 1]?.added || changes[i + 1]?.removed;
-
-		if (!change.added && !change.removed) {
-			// show collapsed
-			if (
-				!options.expand &&
-				change.count !== undefined &&
-				change.count > options.maxUnchanged
-			) {
-				const prevLineOffset = lineOffset;
-				const ls = lines.get(change.value.trimEnd());
-
-				let shownLines = 0;
-
-				if (hasNextChange) shownLines += options.maxUnchanged;
-				if (hasPreviousChange) shownLines += options.maxUnchanged;
-
-				// just show all if we are going to show more than we have
-				if (shownLines >= ls.length) {
-					process.stdout.write(
-						`${lines.join(ls, {
-							prefix: linePrefix,
-						})}\n`
-					);
-					lineOffset += ls.length;
-					continue;
-				}
-
-				// this writes the top few lines
-				if (hasPreviousChange) {
-					process.stdout.write(
-						`${lines.join(ls.slice(0, options.maxUnchanged), {
-							prefix: linePrefix,
-						})}\n`
-					);
-				}
-
-				if (ls.length > shownLines) {
-					const count = ls.length - shownLines;
-					process.stdout.write(
-						`${lines.join(
-							lines.get(
-								color.gray(
-									`+ ${count} more unchanged (${color.italic('-E to expand')})`
-								)
-							),
-							{
-								prefix: () => `${L} ${leftPadMin('', length)} `,
-							}
-						)}\n`
-					);
-				}
-
-				if (hasNextChange) {
-					lineOffset = lineOffset + ls.length - options.maxUnchanged;
-					process.stdout.write(
-						`${lines.join(ls.slice(ls.length - options.maxUnchanged), {
-							prefix: linePrefix,
-						})}\n`
-					);
-				}
-
-				// resets the line offset
-				lineOffset = prevLineOffset + change.count;
-				continue;
-			}
-
-			// show expanded
-
-			process.stdout.write(
-				`${lines.join(lines.get(change.value.trimEnd()), {
-					prefix: linePrefix,
-				})}\n`
-			);
-			lineOffset += change.count ?? 0;
-
-			continue;
-		}
-
-		process.stdout.write(
-			`${lines.join(lines.get(colorChange({ ...change, value: change.value.trimEnd() })), {
-				prefix: linePrefix,
-			})}\n`
-		);
-
-		if (!change.removed) {
-			lineOffset += change.count ?? 0;
-		}
-	}
-};
-
-const colorChange = (change: Change) => {
-	if (change.added) {
-		return color.green(change.value);
-	}
-
-	return color.red(change.value);
+	outro(color.green("All done!"));
 };
 
 export { diff };
---
+++ src/utils/diff.ts
@@ -0,0 +1,151 @@
+import type { Change } from "diff";
+import { arraySum } from "../blocks/utilities/array-sum";
+import * as lines from "../blocks/utilities/lines";
+import { leftPadMin } from "../blocks/utilities/pad";
+import color from "chalk";
+
+type Options = {
+	/** The source file */
+	from: string;
+	/** The destination file */
+	to: string;
+	/** The changes to the file */
+	changes: Change[];
+	/** Expands all lines to show the entire file */
+	expand: boolean;
+	/** Maximum lines to show before collapsing */
+	maxUnchanged: number;
+	/** Color the removed lines */
+	colorRemoved?: (line: string) => string;
+	/** Color the added lines */
+	colorAdded?: (line: string) => string;
+	/** Prefixes each line with the string returned from this function. */
+	prefix: () => string;
+	intro: (options: Options) => string;
+	onUnchanged: (options: Options) => string;
+};
+
+const formatDiff = ({
+	from,
+	to,
+	changes,
+	expand = false,
+	maxUnchanged = 5,
+	colorRemoved = color.red,
+	colorAdded = color.green,
+	prefix,
+	onUnchanged,
+	intro,
+}: Options): string => {
+	let result = "";
+
+	const length = arraySum(changes, (change) => change.count ?? 0).toString().length + 1;
+
+	let lineOffset = 0;
+
+	if (changes.length === 1 && !changes[0].added && !changes[0].removed) {
+		return onUnchanged({
+			from,
+			to,
+			changes,
+			expand,
+			maxUnchanged,
+			colorAdded,
+			colorRemoved,
+			prefix,
+			onUnchanged,
+			intro,
+		});
+	}
+
+	result += intro({ from, to, changes, expand, maxUnchanged, colorAdded, colorRemoved, prefix, onUnchanged, intro });
+
+	/** Provides the line number prefix */
+	const linePrefix = (line: number): string => color.gray(`${prefix?.() ?? ""}${leftPadMin(`${line + 1 + lineOffset} `, length)} `);
+
+	for (let i = 0; i < changes.length; i++) {
+		const change = changes[i];
+
+		const hasPreviousChange = changes[i - 1]?.added || changes[i - 1]?.removed;
+		const hasNextChange = changes[i + 1]?.added || changes[i + 1]?.removed;
+
+		if (!change.added && !change.removed) {
+			// show collapsed
+			if (!expand && change.count !== undefined && change.count > maxUnchanged) {
+				const prevLineOffset = lineOffset;
+				const ls = lines.get(change.value.trimEnd());
+
+				let shownLines = 0;
+
+				if (hasNextChange) shownLines += maxUnchanged;
+				if (hasPreviousChange) shownLines += maxUnchanged;
+
+				// just show all if we are going to show more than we have
+				if (shownLines >= ls.length) {
+					result += `${lines.join(ls, {
+						prefix: linePrefix,
+					})}\n`;
+					lineOffset += ls.length;
+					continue;
+				}
+
+				// this writes the top few lines
+				if (hasPreviousChange) {
+					result += `${lines.join(ls.slice(0, maxUnchanged), {
+						prefix: linePrefix,
+					})}\n`;
+				}
+
+				if (ls.length > shownLines) {
+					const count = ls.length - shownLines;
+					result += `${lines.join(
+						lines.get(color.gray(`+ ${count} more unchanged (${color.italic("-E to expand")})`)),
+						{
+							prefix: () => `${prefix?.() ?? ""}${leftPadMin(" ", length)} `,
+						}
+					)}\n`;
+				}
+
+				if (hasNextChange) {
+					lineOffset = lineOffset + ls.length - maxUnchanged;
+					result += `${lines.join(ls.slice(ls.length - maxUnchanged), {
+						prefix: linePrefix,
+					})}\n`;
+				}
+
+				// resets the line offset
+				lineOffset = prevLineOffset + change.count;
+				continue;
+			}
+
+			// show expanded
+
+			result += `${lines.join(lines.get(change.value.trimEnd()), {
+				prefix: linePrefix,
+			})}\n`;
+			lineOffset += change.count ?? 0;
+
+			continue;
+		}
+
+		const colorChange = (change: Change) => {
+			if (change.added) {
+				return colorAdded(change.value.trimEnd());
+			}
+
+			return colorRemoved(change.value.trimEnd());
+		};
+
+		result += `${lines.join(lines.get(colorChange(change)), {
+			prefix: linePrefix,
+		})}\n`;
+
+		if (!change.removed) {
+			lineOffset += change.count ?? 0;
+		}
+	}
+
+	return result;
+};
+
+export { formatDiff };
