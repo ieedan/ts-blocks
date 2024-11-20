1cf38019a870679c9ffea94b1897819feacbd4c5
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-20 12:44:16 -0600

    add seo tags to demo

--- sites/docs/src/routes/demos/+page.svelte
+++ sites/docs/src/routes/demos/+page.svelte
@@ -2,6 +2,14 @@
 	import { VideoPlayer } from '$lib/components/ui/video-player';
 </script>
 
+<svelte:head>
+	<title>jsrepo ~ Demos</title>
+	<meta
+		name="description"
+		content="jsrepo demos showing off the capabilities of the CLI."
+	/>
+</svelte:head>
+
 <main class="flex justify-center px-4">
 	<div class="max-w-2xl content-center w-full py-8 gap-5 flex justify-center flex-col">
 		<h1 class="text-4xl font-bold text-center">Demos</h1>
