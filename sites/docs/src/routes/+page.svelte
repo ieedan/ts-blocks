0d25263d01e9837774b6f64c39deb4e2f2adb6cf
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-23 05:55:47 -0600

    add youtube video to site and readme

--- sites/docs/src/routes/+page.svelte
+++ sites/docs/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Snippet } from '$lib/components/ui/snippet';
+	import { Button } from '$lib/components/ui/button';
 
 	let { data } = $props();
 </script>
@@ -12,12 +13,10 @@
 	/>
 </svelte:head>
 
-<main
-	class="flex place-items-center justify-center w-full px-4"
-	style="height: calc(100svh - 57px);"
->
+<main class="flex place-items-center justify-center w-full px-4 min-h-[calc(100vh-3.5rem)] pb-10">
 	<div class="max-w-xl w-full">
 		<div class="flex flex-col gap-5 justify-center place-items-center w-full">
+			<section class="h-80"></section>
 			<div class="flex place-items-center gap-4 justify-center w-full">
 				<h1
 					class="bg-primary text-primary-foreground sm:text-6xl text-4xl font-serif font-bold py-4 px-4 w-fit"
@@ -30,6 +29,21 @@
 				The best way to reuse <span class="text-primary">your</span> code.
 			</p>
 			<Snippet command="execute" args={['jsrepo', 'init']} class="max-w-[450px]" />
+			<div class="flex place-items-center gap-2">
+				<Button href="/docs">Get Started</Button>
+				<Button href="/docs/setup/registry" variant="outline">New Registry</Button>
+			</div>
+		</div>
+		<div class="pt-40">
+			<iframe
+				class="aspect-video w-full"
+				src="https://www.youtube.com/embed/IyJQI3z8PWg?si=zhg54w9mE97s6K9c"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe>
 		</div>
 	</div>
 </main>
