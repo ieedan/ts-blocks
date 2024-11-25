85450b224c6d23c4141a576a38680a43125afc10
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-25 11:00:22 -0600

    improve docs

--- sites/docs/src/routes/docs/private-repositories/+page.svelte
+++ sites/docs/src/routes/docs/private-repositories/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
-	import { Blockquote, Code, DocHeader, Jsrepo, Link } from '$lib/components/site/docs';
+	import {
+		Blockquote,
+		Code,
+		DocHeader,
+		Jsrepo,
+		Link,
+		SubHeading,
+		CodeSpan
+	} from '$lib/components/site/docs';
 	import { Snippet } from '$lib/components/ui/snippet';
+	import * as Accordion from '$lib/components/ui/accordion';
 
 	let { data } = $props();
 </script>
@@ -10,7 +19,7 @@
 	description="Yes! jsrepo supports private repositories too."
 />
 <p>
-	Using private repositories is as simple as providing <Jsrepo /> with an auth token.
+	Adding blocks from a private repository is as simple as providing <Jsrepo /> with an auth token.
 </p>
 <Snippet command="execute" args={['jsrepo', 'auth']} />
 <Blockquote variant="primary">
@@ -27,3 +36,19 @@
 │
 └  All done!`}
 />
+<div>
+	<SubHeading>FAQ</SubHeading>
+	<Accordion.Root type="single">
+		<Accordion.Item value="q-1">
+			<Accordion.Trigger>What permissions does my token need?</Accordion.Trigger>
+			<Accordion.Content>
+				<p>
+					For GitHub just create a fine grained token with access to the repositories you are using
+					and make sure to change the access for <CodeSpan>Contents</CodeSpan> under
+					<CodeSpan>Repository permissions</CodeSpan> to
+					<CodeSpan>Read-only</CodeSpan>.
+				</p>
+			</Accordion.Content>
+		</Accordion.Item>
+	</Accordion.Root>
+</div>
